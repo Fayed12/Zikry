a40da70bb8c451949e558b1f54e6e5e186c061e3
mohamed fayed <memad4858@gmail.com>
2025-11-05 19:25:45 +0200

    end rosary logic

--- src/pages/rosary/rosary.jsx
+++ src/pages/rosary/rosary.jsx
@@ -1,28 +1,85 @@
 // react
-import { useState } from "react";
-
-// local
-import styles from "./rosary.module.css";
+import { useEffect, useState } from "react";
 
 // react icons
 import { IoRefreshOutline } from "react-icons/io5";
 import { FaSave } from "react-icons/fa";
 
+// toast
+import toast from "react-hot-toast";
+
+// local
+import styles from "./rosary.module.css";
+import { supabase } from "../../supabaseClient";
+
 const Rosary = () => {
   const [count, setCount] = useState(0);
+  const [userId, setUserId] = useState("");
 
+  // handel increase value
   const handleIncrement = () => {
     setCount((prev) => prev + 1);
   };
 
+  // handle reset counter
   const handleReset = () => {
     setCount(0);
   };
 
-  const handleSave = () => {
-    // Save functionality can be implemented here
+  // handle save count to database
+  async function handleSave() {
+    toast.loading("جاري التحميل....", { id: "rosary-toast" });
+    try {
+      const { data, error } = await supabase
+        .from("users")
+        .update({ rosary_count: count })
+        .eq("id", userId)
+        .select();
+      if (error) {
+        console.log(error);
+        toast.error("حدث خطا ", { id: "rosary-toast" });
+      }
+      toast.success("تم الحفظ", { id: "rosary-toast" });
+      console.log(data);
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
     console.log("Progress saved:", count);
-  };
+  }
+
+  // fetch user id in mount
+  useEffect(() => {
+    const user =
+      JSON.parse(localStorage.getItem("user")) ||
+      JSON.parse(sessionStorage.getItem("user"));
+
+    if (user) {
+      setUserId(user.id);
+    }
+  }, []);
+
+  // handel get user rosary count
+  useEffect(() => {
+    if (userId) {
+      async function fetchUsers() {
+        try {
+          const { data, error } = await supabase
+            .from("users")
+            .select("*")
+            .eq("id", userId);
+          if (error) {
+            console.error(error);
+          }
+          let counter = data.at(0).rosary_count;
+          setCount(counter);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      fetchUsers();
+    }
+  }, [userId]);
 
   return (
     <div className={styles.container}>
